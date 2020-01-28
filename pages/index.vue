f8ff3ecd99ed329d65d84be26e56ba2caaa5469a
Bryan Wong <bryanwhl1999@gmail.com>
2020-01-28 10:07:11 +0800

    added present counter to form UI

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,20 @@
 <template>
   <section class="section">
     <ParadeForm> </ParadeForm>
+    <PresentCounter> </PresentCounter>
   </section>
 </template>
 
 <script>
 import ParadeForm from "~/components/ParadeForm";
+import PresentCounter from "~/components/PresentCounter";
 
 export default {
   name: "HomePage",
 
   components: {
-    ParadeForm
+    ParadeForm,
+    PresentCounter
   }
 };
 </script>
