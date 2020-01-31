62a147d9ef300a1a97d64a87cc66b2a2478f6dfb
Fan Syong Yue <mastersyong@gmail.com>
2020-01-31 16:27:52 +0800

    Clean code

--- components/ParadeForm.vue
+++ components/ParadeForm.vue
@@ -1,57 +1,45 @@
 <template>
-	<section>
-		<div class="background-c">
-			<div
-				class="boxfornameandstatus"
-				v-for="(name, idx) in nameList"
-				:key="idx"
-			>
-				<p style="margin-bottom: 1rem;">{{ name }}</p>
-				<b-field :label="time" v-for="(time, tidx) in daytimeList" :key="tidx">
-					<b-select expanded placeholder="Select Status">
-						<option
-							v-for="(status, sidx) in statusList"
-							:value="status"
-							:key="sidx"
-						>
-							{{ status }}
-						</option>
-					</b-select>
-				</b-field>
-			</div>
-		</div>
-	</section>
+  <section>
+    <div class="boxfornameandstatus" v-for="(name, idx) in nameList" :key="idx">
+      <p style="margin-bottom: 1rem;">{{ name }}</p>
+      <b-field :label="time" v-for="(time, tidx) in daytimeList" :key="tidx">
+        <b-select expanded placeholder="Select Status">
+          <option v-for="(status, sidx) in statusList" :value="status" :key="sidx">{{ status }}</option>
+        </b-select>
+      </b-field>
+    </div>
+  </section>
 </template>
 
 <script>
 import { mapState, mapMutations } from "vuex";
 export default {
-	computed: {
-		...mapState({
-			nameList: state => state.nameList,
-			presentList: state => state.presentList,
-			statusList: state => state.statusList,
-			daytimeList: state => state.daytimeList
-		})
-	},
-	data() {
-		return {
-			radio: "",
-			presentListCurr: []
-		};
-	},
-	methods: {
-		...mapMutations(["addToPresentList"])
-	}
+  computed: {
+    ...mapState({
+      nameList: state => state.nameList,
+      presentList: state => state.presentList,
+      statusList: state => state.statusList,
+      daytimeList: state => state.daytimeList
+    })
+  },
+  data() {
+    return {
+      radio: "",
+      presentListCurr: []
+    };
+  },
+  methods: {
+    ...mapMutations(["addToPresentList"])
+  }
 };
 </script>
 <style>
 .boxfornameandstatus {
-	text-transform: uppercase;
-	background-color: white;
-	padding: 1rem;
-	border-radius: 15px;
-	margin-bottom: 15px;
-	box-shadow: 0 0 1pt 1pt #dadce0;
+  text-transform: uppercase;
+  background-color: white;
+  padding: 1rem;
+  border-radius: 15px;
+  margin-bottom: 15px;
+  box-shadow: 0 0 1pt 1pt #dadce0;
 }
 </style>
