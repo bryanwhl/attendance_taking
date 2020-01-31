945fdec8e23c752fda4cd41c8e6f1a9feb6c9e59
Fan Syong Yue <mastersyong@gmail.com>
2020-01-31 16:27:12 +0800

    use sectionId to get form data

---
+++ pages/form/_sid.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="section">
+    <ParadeForm></ParadeForm>
+    <PresentCounter></PresentCounter>
+  </section>
+</template>
+
+<script>
+import ParadeForm from "~/components/ParadeForm";
+import PresentCounter from "~/components/PresentCounter";
+
+export default {
+  layout: "form",
+  name: "HomePage",
+
+  components: {
+    ParadeForm,
+    PresentCounter
+  }
+};
+</script>
