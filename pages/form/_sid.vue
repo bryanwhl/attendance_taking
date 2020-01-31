985b3cb111175d64685c22420f87907436e4d121
Fan Syong Yue <mastersyong@gmail.com>
2020-01-31 23:06:07 +0800

    pull names from section id

--- pages/form/_sid.vue
+++ pages/form/_sid.vue
@@ -12,7 +12,10 @@ import PresentCounter from "~/components/PresentCounter";
 export default {
   layout: "form",
   name: "HomePage",
-
+  async asyncData({ params, $http }) {
+    const retList = await $http.$get(`getPersonsBySectionId/${params.sid}`);
+    return { personList: retList };
+  },
   components: {
     ParadeForm,
     PresentCounter
