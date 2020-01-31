c6fca2eb82120778d9586cdf193639ce66a0dd9f
Fan Syong Yue <mastersyong@gmail.com>
2020-01-31 16:22:47 +0800

    add proxy to stop CORS errors

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,47 @@
+const api_url = "";
+const host = "0.0.0.0";
 export default {
-	mode: 'universal',
+	mode: "universal",
+	server: {
+		host: host
+	},
 	/*
 	 ** Headers of the page
 	 */
 	head: {
-		title: process.env.npm_package_name || '',
-		meta: [{
-				charset: 'utf-8'
+		title: process.env.npm_package_name || "",
+		meta: [
+			{
+				charset: "utf-8"
 			},
 			{
-				name: 'viewport',
-				content: 'width=device-width, initial-scale=1'
+				name: "viewport",
+				content: "width=device-width, initial-scale=1"
 			},
 			{
-				hid: 'description',
-				name: 'description',
-				content: process.env.npm_package_description || ''
+				hid: "description",
+				name: "description",
+				content: process.env.npm_package_description || ""
 			}
 		],
-		link: [{
-			rel: 'icon',
-			type: 'image/x-icon',
-			href: '/favicon.ico'
-		}]
+		link: [
+			{
+				rel: "icon",
+				type: "image/x-icon",
+				href: "/favicon.ico"
+			}
+		]
 	},
 	/*
 	 ** Customize the progress-bar color
 	 */
 	loading: {
-		color: '#fff'
+		color: "#fff"
 	},
 	/*
 	 ** Global CSS
 	 */
-	css: [],
+	css: ["~/assets/customstyling.scss"],
 	/*
 	 ** Plugins to load before mounting the App
 	 */
@@ -45,12 +53,17 @@ export default {
 	/*
 	 ** Nuxt.js modules
 	 */
-	modules: [
-		'nuxt-buefy',
-		'@nuxt/http'
-	],
+	modules: ["nuxt-buefy", "@nuxt/http"],
 	http: {
-		baseURL: "http://localhost:5000/"
+		proxy: true,
+		prefix: "/api"
+	},
+	proxy: {
+		"/api/": {
+			target:
+				"http://ec2-13-229-122-67.ap-southeast-1.compute.amazonaws.com:5000/",
+			pathRewrite: { "^/api/": "" }
+		}
 	},
 	/*
 	 ** Build configuration
@@ -61,4 +74,4 @@ export default {
 		 */
 		extend(config, ctx) {}
 	}
-}
+};
